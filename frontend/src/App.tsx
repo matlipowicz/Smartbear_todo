ca4e93921f052a4216d7ab5fa437fe154e97ffc0
Mateusz Lipowicz <lipusek1997@gmail.com>
2023-10-31 08:57:13 +0100

    config: base configuration vite project with tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="text-2xl font-bold underline " style={{ fontFamily: 'Lato, sans-serif' }}>
+            Hello TODO app!
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,34 @@
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'bright-purple-100': '#8687E7',
+                'bright-puprle-200': '#8875FF',
+                'bright-puprle-300': '#8E7CFF',
+
+                'gray-100': '#979797',
+                'gray-200': '#363636',
+                'gray-300': '#272727',
+                'gray-400': '#1D1D1D',
+
+                'red-100': '#FF4949',
+                'category-yellow-100': '#C9CC41',
+                'category-yellow-200': '#FFCC80',
+                'category-greem': '#66CC41',
+                'category-turquiose': '#41CCA7',
+                'category-blue-100': '#4181CC',
+                'category-blue-200': '#41A2CC',
+                'category-orange': '#CC8441',
+                'category-purple-100': '#9741CC',
+                'category-purple-200': '#809CFF',
+                'category-red': '#CC4173',
+                'test-black': '#121212',
+            },
+            fontFamily: {
+                lato: ['Lato', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
